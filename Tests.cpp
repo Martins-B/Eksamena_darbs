129b8f22fca52a695c0bad8208bf516202c0c5c0
Martins-B <[martinsbardulis@gmail.com]>
2021-06-17 02:58:20 +0300

    Pabeigta testa izstrāde un veikt koda komentēšana

--- Tests.cpp
+++ Tests.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <string>
-#include <fstream>
 using namespace std;
 int main(){
-	string jautajumi [10] = {"1. Kura no funkcijam nenoapalo veselu skaitli uz leju?",
+	string jautajumi [10] = {"1. Kura no funkcijam nenoapalo veselu skaitli uz leju?",	// Izveido masîvu un aizpilda to ar testa jautâjumiem.
 	  "2. Kadu rezultatu nevar iegut noapalojot 2.3 ar round() funkciju?",
 	  "3. Kadu funkciju jaizmanto, lai noapalojot 4.6 var iegut 5.0?",
 	  "4. Kura no definicijam neatbilst funkcijai max(x,y)?",
@@ -11,12 +10,10 @@ int main(){
 	  "6. Bez kura virsrakstfaila nav iespejams izmantot funkciju rand()?",
 	  "7. Kurs funkcijas rand() piemers generes nejausu skaitli intervala no 0 lîdz 22?",
 	  "8. Kura no funkcijam neatgriez skaitla moduli?",
-	  "9. Kuru no pow() funkcijam jaizmanto, lai iegûtu rezultatu 16?",
+	  "9. Kuru no pow() funkcijam jaizmanto, lai iegutu rezultatu 16?",
 	  "10. Kads skaitlis ietilps funkcijas rand()%15-4 generetaja intervla?"};
-	  
-		string nepareiziJautajumi [10];
 	
-	string atbildesVarianti [10] = {"A)round()\nB)ceil()\nC)floor()\nD)rand()\n",
+	string atbildesVarianti [10] = {"A)round()\nB)ceil()\nC)floor()\nD)rand()\n",	// Izveido masîvu un aizpilda to ar testa jautâjumu atbilşu variantiem.
 	"A)2.0\nB)3.0\nC)2.5\nD)3.3\n",
 	"A)round()\nB)ceil()\nC)floor()\nD)trunc()\n",
 	"A)max(x,y) - nosaka mazako no 2 skaitliem.\nB)max(x,y) - nosaka starpibu starp x un y.\nC)max(x,y) - kapina x pakape y.\nD)max(x,y) - nosaka lielako vertibu no 2 skaitliem.\n",
@@ -27,80 +24,83 @@ int main(){
 	"A)pow(3,4)\nB)pow(2,4)\nC)pow(16,1)\nD)pow(8,2)\n",
 	"A)-4\nB)8\nC)11\nD)-15\n"}; 
 
-	int pareizoSkaits=0;
-	string atbilde [10];
+	cout<<"Sveicinati testa, lai parbauditu Jusu zinasanas par matematiskajam funkcijam!\nLai atbildetu, ievadiet atbilzu varianta burtu.\nLai veicas!\n";	//Izvada paziòojumu, lai iepazîstinâtu ar testu.
+
+	string atbilde [10];	//Izveido masîvu kurâ uzglabâs lietotâja ievadîtâs atzîmes.
 	
-	  for(int i=0;i<10;i++){
-	  	cout<<"\n"<<jautajumi[i]<<"\n";
-	  	cout<<atbildesVarianti [i]<<"\n";
-	  	cout<<"Tava atbilde:";
-	  	cin>>atbilde[i];
+	  for(int i=0;i<10;i++){	//Izveido ciklu ar skaitîtâju, kurğ nostrâdâs 10 reizes.
+	  	cout<<"\n"<<jautajumi[i]<<"\n";	//Izvada jautâjumu.
+	  	cout<<atbildesVarianti [i]<<"\n";	//Izvada atbilşu variantus.
+	  	cout<<"Tava atbilde:";	//Prasa lietotâjam ievadît savu atbildi.
+	  	cin>>atbilde[i];	//Nolasa lietotâja ievadîto atbildi
 		}
 		
-	
-	if(atbilde[0]== "a" || atbilde[0]== "A" || atbilde[0]== "b" || atbilde[0]== "B" || atbilde[0]== "d" || atbilde[0]== "D"){
-		pareizoSkaits++;
+	int pareizoSkaits=0;	//Izveido mainîgo, lai skaitîtu pareizi atbildçtus jautâjumus un pieğíir tam vçrtîbu 0.
+	string nepareiziJautajumi [10];	//Izveido masîvu, kurâ uzglabâs nepareizi atbildçtus jautâjumus.
+		
+	if(atbilde[0]== "a" || atbilde[0]== "A" || atbilde[0]== "b" || atbilde[0]== "B" || atbilde[0]== "d" || atbilde[0]== "D"){	//Pârbauda vai lietotâja ievadîta atbilde sakrît ar pareizo atbildi.
+		pareizoSkaits++;	// Ja lietotâjs atbildçjis pareizi, tad palielina pareizi atbildçto jautâjumu skaitu par 1.
 	}else{
-		nepareiziJautajumi[0]=jautajumi[0];
+		nepareiziJautajumi[0]=jautajumi[0];	//Ja lietotâjs atbildçjis nepareizi, tad ğo jautâjumu pievieno masîvam, kurâ uzglabâ nepareizi atbildçtos jautâjumus.
 	}
 	
-	if(atbilde[1]=="c" || atbilde[1]=="C" || atbilde[1]=="b" || atbilde[1]=="B" || atbilde[1]=="d" || atbilde[1]=="D"){
-		pareizoSkaits++;
+	if(atbilde[1]=="c" || atbilde[1]=="C" || atbilde[1]=="b" || atbilde[1]=="B" || atbilde[1]=="d" || atbilde[1]=="D"){	//Pârbauda vai lietotâja ievadîta atbilde sakrît ar pareizo atbildi.
+		pareizoSkaits++;	// Ja lietotâjs atbildçjis pareizi, tad palielina pareizi atbildçto jautâjumu skaitu par 1.
 	}else{
-		nepareiziJautajumi[1]=jautajumi[1];
+		nepareiziJautajumi[1]=jautajumi[1];	//Ja lietotâjs atbildçjis nepareizi, tad ğo jautâjumu pievieno masîvam, kurâ uzglabâ nepareizi atbildçtos jautâjumus.
 	}
 	
-	if(atbilde[2]=="a" || atbilde[2]=="A" || atbilde[2]=="b" || atbilde[2]=="B"){
-		pareizoSkaits++;
+	if(atbilde[2]=="a" || atbilde[2]=="A" || atbilde[2]=="b" || atbilde[2]=="B"){	//Pârbauda vai lietotâja ievadîta atbilde sakrît ar pareizo atbildi.
+		pareizoSkaits++;	// Ja lietotâjs atbildçjis pareizi, tad palielina pareizi atbildçto jautâjumu skaitu par 1.
 	}else{
-		nepareiziJautajumi[2]=jautajumi[2];
+		nepareiziJautajumi[2]=jautajumi[2];	//Ja lietotâjs atbildçjis nepareizi, tad ğo jautâjumu pievieno masîvam, kurâ uzglabâ nepareizi atbildçtos jautâjumus.
 	}
 	
-	if(atbilde[3]=="a" || atbilde[3]=="A" || atbilde[3]=="b" || atbilde[3]=="B" || atbilde[3]=="c" || atbilde[3]=="C"){
-		pareizoSkaits++;
+	if(atbilde[3]=="a" || atbilde[3]=="A" || atbilde[3]=="b" || atbilde[3]=="B" || atbilde[3]=="c" || atbilde[3]=="C"){	//Pârbauda vai lietotâja ievadîta atbilde sakrît ar pareizo atbildi.
+		pareizoSkaits++;	// Ja lietotâjs atbildçjis pareizi, tad palielina pareizi atbildçto jautâjumu skaitu par 1.
 	}else{
-		nepareiziJautajumi[3]=jautajumi[3];
+		nepareiziJautajumi[3]=jautajumi[3];	//Ja lietotâjs atbildçjis nepareizi, tad ğo jautâjumu pievieno masîvam, kurâ uzglabâ nepareizi atbildçtos jautâjumus.
 	}
 	
-	if(atbilde[4]=="a" || atbilde[4]=="A" || atbilde[4]=="c" || atbilde[4]=="C"){
-		pareizoSkaits++;
+	if(atbilde[4]=="a" || atbilde[4]=="A" || atbilde[4]=="c" || atbilde[4]=="C"){	//Pârbauda vai lietotâja ievadîta atbilde sakrît ar pareizo atbildi.
+		pareizoSkaits++;	// Ja lietotâjs atbildçjis pareizi, tad palielina pareizi atbildçto jautâjumu skaitu par 1.
 	}else{
-		nepareiziJautajumi[4]=jautajumi[4];
+		nepareiziJautajumi[4]=jautajumi[4];	//Ja lietotâjs atbildçjis nepareizi, tad ğo jautâjumu pievieno masîvam, kurâ uzglabâ nepareizi atbildçtos jautâjumus.
 	}
 	
-	if(atbilde[5]=="b" || atbilde[5]=="B" || atbilde[5]=="d" || atbilde[5]=="D"){
-		pareizoSkaits++;
+	if(atbilde[5]=="b" || atbilde[5]=="B" || atbilde[5]=="d" || atbilde[5]=="D"){	//Pârbauda vai lietotâja ievadîta atbilde sakrît ar pareizo atbildi.
+		pareizoSkaits++;	// Ja lietotâjs atbildçjis pareizi, tad palielina pareizi atbildçto jautâjumu skaitu par 1.
 	}else{
-		nepareiziJautajumi[5]=jautajumi[5];
+		nepareiziJautajumi[5]=jautajumi[5];	//Ja lietotâjs atbildçjis nepareizi, tad ğo jautâjumu pievieno masîvam, kurâ uzglabâ nepareizi atbildçtos jautâjumus.
 	}
 	
-	if(atbilde[6]=="b" || atbilde[6]=="B" || atbilde[6]=="a" || atbilde[6]=="A"){
-		pareizoSkaits++;
+	if(atbilde[6]=="b" || atbilde[6]=="B" || atbilde[6]=="a" || atbilde[6]=="A"){	//Pârbauda vai lietotâja ievadîta atbilde sakrît ar pareizo atbildi.
+		pareizoSkaits++;	// Ja lietotâjs atbildçjis pareizi, tad palielina pareizi atbildçto jautâjumu skaitu par 1.
 	}else{
-		nepareiziJautajumi[6]=jautajumi[6];
+		nepareiziJautajumi[6]=jautajumi[6];	//Ja lietotâjs atbildçjis nepareizi, tad ğo jautâjumu pievieno masîvam, kurâ uzglabâ nepareizi atbildçtos jautâjumus.
 	}
 	
-	if(atbilde[7]=="a" || atbilde[7]=="A" || atbilde[7]=="b" || atbilde[7]=="B" || atbilde[7]=="d" || atbilde[7]=="D"){
-		pareizoSkaits++;
+	if(atbilde[7]=="a" || atbilde[7]=="A" || atbilde[7]=="b" || atbilde[7]=="B" || atbilde[7]=="d" || atbilde[7]=="D"){	//Pârbauda vai lietotâja ievadîta atbilde sakrît ar pareizo atbildi.
+		pareizoSkaits++;	// Ja lietotâjs atbildçjis pareizi, tad palielina pareizi atbildçto jautâjumu skaitu par 1.
 	}else{
-		nepareiziJautajumi[7]=jautajumi[7];
+		nepareiziJautajumi[7]=jautajumi[7];	//Ja lietotâjs atbildçjis nepareizi, tad ğo jautâjumu pievieno masîvam, kurâ uzglabâ nepareizi atbildçtos jautâjumus.
 	}
 	
-	if(atbilde[8]=="c" || atbilde[8]=="C" || atbilde[8]=="b" || atbilde[8]=="B"){
-		pareizoSkaits++;
+	if(atbilde[8]=="c" || atbilde[8]=="C" || atbilde[8]=="b" || atbilde[8]=="B"){	//Pârbauda vai lietotâja ievadîta atbilde sakrît ar pareizo atbildi.
+		pareizoSkaits++;	// Ja lietotâjs atbildçjis pareizi, tad palielina pareizi atbildçto jautâjumu skaitu par 1.
 	}else{
-		nepareiziJautajumi[8]=jautajumi[8];
+		nepareiziJautajumi[8]=jautajumi[8];	//Ja lietotâjs atbildçjis nepareizi, tad ğo jautâjumu pievieno masîvam, kurâ uzglabâ nepareizi atbildçtos jautâjumus.
 	}
 	
-	if(atbilde[9]=="a" || atbilde[9]=="A" || atbilde[9]=="b" || atbilde[9]=="B"){
-		pareizoSkaits++;
+	if(atbilde[9]=="a" || atbilde[9]=="A" || atbilde[9]=="b" || atbilde[9]=="B"){	//Pârbauda vai lietotâja ievadîta atbilde sakrît ar pareizo atbildi.
+		pareizoSkaits++;	// Ja lietotâjs atbildçjis pareizi, tad palielina pareizi atbildçto jautâjumu skaitu par 1.
 	}else{
-		nepareiziJautajumi[9]=jautajumi[9];
+		nepareiziJautajumi[9]=jautajumi[9];	//Ja lietotâjs atbildçjis nepareizi, tad ğo jautâjumu pievieno masîvam, kurâ uzglabâ nepareizi atbildçtos jautâjumus.
 	}
 	
-	cout<<"\nApsveicu, Tu atbildeji pareizi uz "<<pareizoSkaits<<" jautajumiem!\n";
-	cout<<"\nDiemzel, Tu atbildeji nepareizi uz siem jautajumiem: \n";
-	for (int i=0; i<10; i++){
-		cout<<"\n"<<nepareiziJautajumi[i];
+	cout<<"\nApsveicu, Tu atbildeji pareizi uz "<<pareizoSkaits<<" jautajumiem!\n";	//Izvada mainîgo, kurâ saglabâts pareizi atbildçto jautâjumu skaits.
+	cout<<"\nDiemzel, Tu atbildeji nepareizi uz siem jautajumiem: \n";	//Izvada paziòojumu par nepareizi atbildçtiem jautâjumiem.
+	for (int i=0; i<10; i++){	//Izveido ciklu ar skaitîtâju, lai izvadîtu nepareizi atbildçtos jautâjumus.
+		cout<<"\n"<<nepareiziJautajumi[i];	//Izvada masîvu, kurâ saglabâti jautâjumi uz kuriem atbildçts nepareizi.
 	}
 }
