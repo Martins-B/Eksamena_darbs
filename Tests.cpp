baef06c06082b62742145ab2b1714deb8c461c7d
Martins-B <[martinsbardulis@gmail.com]>
2021-06-17 22:21:03 +0300

    Paskaidorta pēdējā koda rinda

--- Tests.cpp
+++ Tests.cpp
@@ -103,5 +103,5 @@ int main(){
 	for (int i=0; i<10; i++){	//Izveido ciklu ar skaitîtâju, lai izvadîtu nepareizi atbildçtos jautâjumus.
 		cout<<"\n"<<nepareiziJautajumi[i];	//Izvada masîvu, kurâ saglabâti jautâjumi uz kuriem atbildçts nepareizi.
 	}
-	system("pause");
+	system("pause"); //Neïauj programmai uzreiz aizvçrties pçc koda izpildes, lai lietotâjs redzçtu testa rezultâtus.
 }
